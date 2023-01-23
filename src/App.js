183bbeb08b8052846eb76222b1953ee6b8e757d1
Pavle Marinkovic <payche255@gmail.com>
2023-01-23 14:48:20 +0100

    Update 404 page & Routes fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/header/Navbar";
 import Home from "./components/home/Home";
 import Create from "./components/create/Create";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BlogDetails from "./components/create/BlogDetails";
 import NotFound from "./components/404/NotFound";
 
@@ -13,12 +13,12 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-          </Routes>
-          <Routes>
+
             <Route path="/create" element={<Create />} />
-          </Routes>
-          <Routes>
+
             <Route path="/blogs/:id" element={<BlogDetails />} />
+
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
